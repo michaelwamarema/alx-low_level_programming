a2af90c83ebff858b9c74f0cee330ea4248886de
Michael Wamarema <michael@databit.net>
2023-03-31 08:35:20 -0700

    function that appends the src string to the dest string, overwriting the terminating null byte (\0) at the end of dest, and then adds a terminating null byte

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include main.h
+/**
+ * _strcat - concatenates two arrays
+ *
+ * @dest - destination of concatenation
+ * @src - source of concatenation
+ *
+ * return char value
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (j=0; dest[] != '\0'; j++)
+	{}
+	for (i=0; src[i] != '\0'; i++)
+	{}
+
+	dest[j + 1] = '\0';
+
+	return(dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
+/**
+ * _strcat - concatenates strings
+ * @dest - destination of string
+ * @src - source of string
+ *
+ * return char value
+ */
+char*_strcat(char *dest, char *src);
