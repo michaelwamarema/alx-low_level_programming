740ae9ccfa6204728a9e66ff761b02a51483f0d5
Michael Wamarema <michael@databit.net>
2023-04-03 10:58:27 -0700

    changes to file 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,8 @@
  * @s: array to set
  * @b: value to set
  * @n: amount of times
- * return: char value of s
+ * Description: fills memory s with byte b for n amount
+ * Return: char value of s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
