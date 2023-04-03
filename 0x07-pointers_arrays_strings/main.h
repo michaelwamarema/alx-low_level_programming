aaf573a3c63122385de0c814bcfbcc32d6a70725
Michael Wamarema <michael@databit.net>
2023-04-03 10:52:06 -0700

    function that fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _memset - sets memory to array
+ * @s: array to set
+ * @b: value to set
+ * @n: amount of times
+ * return: char value of s
+ */
+char *_memset(char *s, char b, unsigned int n);
